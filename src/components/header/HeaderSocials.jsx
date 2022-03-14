37ac04ac1a90263a6413c82e934a123c6bd1d61b
davidadeola_ <davidolorunsesan15@gmail.com>
2022-03-14 07:06:25 +0100

    update social commit

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,9 +5,9 @@ import {FaGithub} from 'react-icons/fa';
 const HeaderSocials = () => {
   return (
       <div className="header__socials">
-          <a href="https://linkedin.com" target='_blank'><BsLinkedin /></a>
-          <a href="https://github.com" target='_blank'><FaGithub /></a>
-          <a href="https://twitter.com" target='_blank'><BsTwitter /></a>
+          <a href="https://linkedin.com/in/david-adeola" target='_blank'><BsLinkedin /></a>
+          <a href="https://github.com/davidadeola" target='_blank'><FaGithub /></a>
+          <a href="https://twitter.com/DavidAdeola_" target='_blank'><BsTwitter /></a>
       </div>
   )
 }
