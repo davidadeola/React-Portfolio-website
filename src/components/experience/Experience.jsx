9908558df9a549575f259217803c4977e3096cb5
davidadeola_ <davidolorunsesan15@gmail.com>
2022-03-16 13:31:34 +0100

    New sectio commit

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,7 +3,17 @@ import './Experience.css';
 
 function Experience() {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skill I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend"></div>
+        <div className="experience__backend">
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
